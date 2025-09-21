45b0d6fac2c438fcd79d9c8404ce6670116bc0a5
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2025-09-21 13:10:06 +0300

    page

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -3,7 +3,6 @@ import { cookies } from "next/headers";
 import { api } from "../../api";
 import { parse } from "cookie";
 import { isAxiosError } from "axios";
-// import { logErrorResponse } from "../../_utils/utils";
 
 export async function GET() {
     try {
