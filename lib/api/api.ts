94a56faa74f481c43ed4173dfa44f7caf37d674f
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2025-10-06 01:00:55 +0300

    https:/study

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,16 +1,16 @@
-// import axios from "axios";
-
-// export const nextServer = axios.create({
-//     baseURL:
-//         (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/' + 'api'),
-//     withCredentials: true,
-// });
-
-
 import axios from "axios";
 
 export const nextServer = axios.create({
     baseURL:
-        (process.env.NEXT_PUBLIC_API_URL || 'https://notehub-api.goit.study/docs' + 'api'),
+        (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/' + 'api'),
     withCredentials: true,
 });
+
+
+// import axios from "axios";
+
+// export const nextServer = axios.create({
+//     baseURL:
+//         (process.env.NEXT_PUBLIC_API_URL || 'https://notehub-api.goit.study/docs' + 'api'),
+//     withCredentials: true,
+// });
